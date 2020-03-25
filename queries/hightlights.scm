916f987b59c6183e2e422dfcb585221e60014bd1
GrayJack <gr41.j4ck@gmail.com>
2020-03-25 19:39:20 -0300

    Add basic highlight file

---
+++ queries/hightlights.scm
@@ -0,0 +1,39 @@
+
+"if" @keyword
+"do" @keyword
+"fn" @keyword
+"def" @keyword
+"let" @keyword
+"ref" @keyword
+"the" @keyword
+"use" @keyword
+"set!" @keyword
+"defn" @keyword
+"load" @keyword
+"with" @keyword
+"while" @keyword
+"match" @keyword
+"address" @keyword
+"deftype" @keyword
+"defmacro" @keyword
+"defmodule" @keyword
+"defdynamic" @keyword
+"definterface" @keyword
+"local-include" @keyword
+"register-type" @keyword
+"system-include" @keyword
+
+(short_copy "@" @support.copy)
+(short_ref "&" @support.reference)
+(short_fn_ref "~" @support.reference)
+
+(escape_sequence) @escape
+
+(str_literal) @string
+(char_literal) @string
+(pattern_literal) @string
+
+(unit) @constant.builtin
+(bool_literal) @constant.builtin
+(float_literal) @constant.builtin
+(integer_literal) @constant.builtin
